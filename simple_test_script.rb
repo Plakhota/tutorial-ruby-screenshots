86aa94d32326f035f4e3578777919b1567670516
Colby Fayock <fayock@gmail.com>
2021-06-07 16:11:02 -0400

    updating test image

--- simple_test_script.rb
+++ simple_test_script.rb
@@ -2,7 +2,7 @@ require 'eyes_images'
 
 runner = Applitools::ClassicRunner.new
 eyes = Applitools::Images::Eyes.new(runner: runner)
-batch = Applitools::BatchInfo.new('Applitools Screenshot example')
+batch = Applitools::BatchInfo.new('Demo Batch - Images - Ruby')
 
 eyes.batch = batch
 
@@ -11,27 +11,16 @@ Applitools::EyesLogger.log_handler = Logger.new(STDOUT).tap do |l|
 end
 
 begin
-  file_path = './PNG_IMG1.png'
+  file_path = './applitools.png'
   image_bytes = File.read(file_path)
   image = Applitools::Screenshot.from_datastream(image_bytes)
 
-  # Classic API
-  eyes.open(app_name: 'Screenshot example app', test_name: 'Screenshot example classic api')
-  eyes.check_image(tag: 'By file path', image_path: file_path)
-  eyes.check_image(tag: 'By image bytes', image_bytes: image_bytes)
-  eyes.check_image(tag: 'By Applitools Screenshot', image: image)
-  eyes.check_region(tag: 'Check region example', image: image, region: Applitools::Region.new(200, 200, 100, 100))
-  eyes.close
-
-  # Fluent API
-  eyes.open(app_name: 'Screenshot example app', test_name: 'Screenshot example fluent api')
+  eyes.open(app_name: 'Demo App - Images Ruby', test_name: 'Smoke Test - Images Ruby')
+  
   eyes.check('By file path', Applitools::Images::Target.path(file_path))
   eyes.check('By image bytes', Applitools::Images::Target.blob(image_bytes))
   eyes.check('By Applitools Screenshot', Applitools::Images::Target.screenshot(image))
-  eyes.check(
-    'Check region example',
-    Applitools::Images::Target.screenshot(image).region(Applitools::Region.new(200, 200, 100, 100))
-  )
+
   eyes.close
 rescue => e
   puts e.message
@@ -39,5 +28,3 @@ rescue => e
 ensure
   puts runner.get_all_test_results
 end
-
-
